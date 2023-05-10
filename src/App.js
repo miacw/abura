da14d215617054da7951e575e63533291bdacd06
miacw_ <miawallace1999@hotmail.co.uk>
2023-05-10 15:39:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import "./App.css";
 function App() {
   function triggerForm() {
     window._klOnsite = window._klOnsite || [];
-    window._klOnsite.push(["openForm", "9206885"]);
+    window._klOnsite.push(["openForm", "R9uZQE"]);
   }
   return (
     <div className="container">
-      <h1> Hello World</h1>
       <button onClick={triggerForm}>Stay in the Loop</button>
     </div>
   );
